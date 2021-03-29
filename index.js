15913b796991859e8b52d1d540b9228d1f1f8481
Jimmy Mena <angie@novazys.com>
2021-03-29 01:06:56 -0600

    (bad): bad version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const request = require("request-promise");
 const cheerio = require("cheerio");
 const MongoClient = require('mongodb').MongoClient;
@@ -18,17 +18,16 @@ var database, collection, bulk;
 
 
 app.listen(5000, () => {
-	client.connect( (err, database) => {
+	client.connect( async (err, database) => {
 		collection = client.db("postal_codes_nicaragua").collection("postal_codes_Nicaragua");
 		bulk = collection.initializeUnorderedBulkOp();
 		db = database;
 		// $** allow search for all fields
-    	const result = collection.createIndex(
+    	const result = await collection.createIndex(
     		{  "$**": "text" },
     		{ default_language: "spanish" }
     	);
 
-		console.log("Hii Nativo Software, you are connected to `" + DB_COLLECTION_NAME + "`!");
 		console.log("Hii Nativo Software, you are connected to `" + DATABASE_NAME + "`!");
 		// perform actions on the collection object
 		// client.close();
@@ -36,34 +35,30 @@ app.listen(5000, () => {
 });
 
 app.get("/insertPostalCodes", async (request, response) => {
-	try {
-		const postal_codes = await getPostalCodesFromWeb();
-		const $ = cheerio.load(postal_codes);
-		const scrapedData = [];
- 
-		$("#mw-content-text > div > table > tbody > tr").each( (index, element) => {
-			if (index === 0) return true;
-			const tds = $(element).find("td");
-			const Deparment = $(tds[0]).text().replace(/\n/g, '');
-			const Municipality = $(tds[1]).text().replace(/\n/g, '');
-			const Postal_code = $(tds[2]).text().replace(/\n/g, '');
-			const Neighbourhood = $(tds[3]).text().replace(/\n/g, '');
-			// Build Object and insert collection
-	        const tableRow = { Deparment, Municipality, Postal_code, Neighbourhood };
-	    	scrapedData.push(tableRow);
-		});
-		
-		collection.insert( scrapedData , (error, result) => {       
-			console.log(error); 
-			if(error) {
-				return response.status(500).send(error);
-			}
-		    response.send(result.result);
-		});
-		
-	} catch (error) {
-		console.log(error);
-	}
+
+	const postal_codes = await getPostalCodesFromWeb();
+	const $ = cheerio.load(postal_codes);
+	const scrapedData = [];
+
+	$("#mw-content-text > div > table > tbody > tr").each( (index, element) => {
+		if (index === 0) return true;
+		const tds = $(element).find("td");
+		const Deparment = $(tds[0]).text().replace(/\n/g, '');
+		const Municipality = $(tds[1]).text().replace(/\n/g, '');
+		const Postal_code = $(tds[2]).text().replace(/\n/g, '');
+		const Neighbourhood = $(tds[3]).text().replace(/\n/g, '');
+		// Build Object and insert collection
+        const tableRow = { Deparment, Municipality, Postal_code, Neighbourhood };
+    	scrapedData.push(tableRow);
+	});
+	
+	collection.insert( scrapedData , (error, result) => {       
+		console.log(error); 
+		if(error) {
+			return response.status(500).send(error);
+		}
+	    response.send(result.result);
+	});
 });
 
 app.post("/search", (request, response) => {
@@ -82,9 +77,5 @@ app.post("/search", (request, response) => {
 
 
 async function getPostalCodesFromWeb() {
-	try {
-	 	return await request.get("https://en.wikipedia.org/wiki/Postal_codes_in_Nicaragua");
-	} catch (error){
-		console.log(error);
-	}
+ 	return await request.get("https://en.wikipedia.org/wiki/Postal_codes_in_Nicaragua");
 }
